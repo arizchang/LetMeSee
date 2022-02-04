8faad17ea7754c543a99af98c0cabf646942b9d9
arizchang <abchang@asu.edu>
2022-02-03 17:44:47 -0700

    added dattebayo button

--- app/src/main/java/com/example/letmesee/MainActivity.kt
+++ app/src/main/java/com/example/letmesee/MainActivity.kt
@@ -9,25 +9,42 @@ import android.widget.Button
 private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
     private lateinit var btPlay: Button
+    private lateinit var btDattebayo: Button
     private lateinit var soundPool: SoundPool
-    private var soundStreamId: Int = 0
+    private var soundStreamIdLms: Int = 0
+    private var soundStreamIdDattebayo: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btPlay = findViewById(R.id.btPlay)
+        btDattebayo = findViewById(R.id.btDattebayo)
         soundPool = SoundPool.Builder().build()
-        val soundId = soundPool.load(this, R.raw.let_me_see, 1)
+        val soundIdLms = soundPool.load(this, R.raw.let_me_see, 1)
+        val soundIdDattebayo = soundPool.load(this, R.raw.dattebayo, 1)
 
         btPlay.setOnClickListener {
-            Log.i(TAG, "clicked test")
+            Log.i(TAG, "let me see")
 
-            soundStreamId = if (soundStreamId == 0) {
-                soundPool.play(soundId, 1F, 1F, 0, 0, 1F)
+            soundStreamIdLms = if (soundStreamIdLms == 0) {
+                soundPool.play(soundIdLms, 1F, 1F, 0, 0, 1F)
             } else {
-                soundPool.pause(soundStreamId)
+                soundPool.pause(soundStreamIdLms)
                 0
             }
+            soundStreamIdLms = 0
+        }
+
+        btDattebayo.setOnClickListener {
+            Log.i(TAG, "dattebayo")
+
+            soundStreamIdDattebayo = if (soundStreamIdDattebayo == 0) {
+                soundPool.play(soundIdDattebayo, 1F, 1F, 0, 0, 1F)
+            } else {
+                soundPool.pause(soundStreamIdDattebayo)
+                0
+            }
+            soundStreamIdDattebayo = 0
 
         }
     }
